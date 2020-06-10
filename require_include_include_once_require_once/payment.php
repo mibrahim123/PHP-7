4a75f90cecb7a42851794b5fb2a2b06e697e53c7
Muhammad Ibrahim <mibrahim.coderkube@gmail.com>
2020-06-10 14:23:10 +0530

    files for require include

---
+++ require_include_include_once_require_once/payment.php
@@ -0,0 +1,12 @@
+<?php
+    $payment=[
+        [["product_id"=>1],["payment_id"=>1],["price"=>1000]],
+        [["product_id"=>2],["payment_id"=>2],["price"=>1000]],
+        [["product_id"=>3],["payment_id"=>3],["price"=>2000]],
+        [["product_id"=>4],["payment_id"=>4],["price"=>3000]],
+        [["product_id"=>5],["payment_id"=>5],["price"=>4000]],
+        
+    
+    ];
+
+?>
---
+++ require_include_include_once_require_once/products.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$product=[
+        [["product_id"=>1],["product_name"=>"Dell Xpire"]],
+        [["product_id"=>2],["product_name"=>"Redmi Note4"]],
+        [["product_id"=>3],["product_name"=>"Redmi 7"]],
+        [["product_id"=>4],["product_name"=>"Apple Macbook Pro"]],
+        [["product_id"=>5],["product_name"=>"Iphone MAx PRO"]],
+        
+    
+    ];
+?>
